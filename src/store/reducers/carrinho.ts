9a90f184b03aaa807f65377c9997e1c864675cd6
Tiago Biazin <tiago.biazin02@gmail.com>
2024-05-16 14:16:27 -0300

    commit inicial

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,44 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+  favoritos: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: [],
+  favoritos: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    },
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.favoritos.find((p) => p.id === produto.id)) {
+        const favoritosSemProduto = state.favoritos.filter(
+          (p) => p.id !== produto.id
+        )
+
+        state.favoritos = favoritosSemProduto
+      } else {
+        state.favoritos.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar, favoritar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
