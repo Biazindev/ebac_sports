57957e192c0360eebeaa0f3c06e3398874085005
Tiago Biazin <tiago.biazin02@gmail.com>
2024-05-23 11:45:20 -0300

    corrigindo erros API

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,13 +1,10 @@
-import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
 import { useGetProdutosQuery } from '../services/api'
 import * as S from './styles'
 
 const ProdutosComponent = () => {
   const { data: produtos, isLoading } = useGetProdutosQuery()
-
   if (isLoading) return <h2>Carregando...</h2>
-
   return (
     <>
       <S.Produtos>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Produto } from '../App'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app'
+    baseUrl: 'https://fake-api-tau.vercel.app/api/ebac_sports'
   }),
   endpoints: (builder) => ({
     getProdutos: builder.query<Produto[], void>({
